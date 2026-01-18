798df61eeda52b02fec8fefbef6d45712fbac2c1
Arbazx01 <mdarbaz9675@gmail.com>
2026-01-18 17:30:35 +0530

    fix theme-provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,9 +2,27 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-// We import the specific type for the props here
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+interface MyThemeProviderProps {
+    children: React.ReactNode;
+    attribute?: "class" | "data-theme";
+    defaultTheme?: string;
+    enableSystem?: boolean;
+}
+
+export function ThemeProvider({
+    children,
+    attribute = "class",
+    defaultTheme = "system",
+    enableSystem = true,
+}: MyThemeProviderProps) {
+    return (
+        <NextThemesProvider
+            attribute={attribute}
+            defaultTheme={defaultTheme}
+            enableSystem={enableSystem}
+        >
+            {children}
+        </NextThemesProvider>
+    );
 }
